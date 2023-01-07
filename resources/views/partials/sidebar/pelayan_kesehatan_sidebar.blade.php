6ae1b6aef947e35da310f635a132e039c3e67928
christian-alexander <christian082233@gmail.com>
2023-01-07 16:26:23 +0700

    sesuaikan pelayan_kesehatan_sidebar

--- resources/views/partials/sidebar/pelayan_kesehatan_sidebar.blade.php
+++ resources/views/partials/sidebar/pelayan_kesehatan_sidebar.blade.php
@@ -9,40 +9,16 @@
     <!-- Dashboard -->
     <li class="menu-item active">
       <a href="#" class="menu-link">
-        <i class="fa-solid fa-house fa-lg"></i>
+        {{-- <i class="fa-solid fa-house fa-lg"></i> --}}
         <div data-i18n="Analytics">Dashboard</div>
       </a>
     </li>
 
-    <!-- Kelas -->
+    <!-- Pasien -->
     <li class="menu-item">
       <a href="#" class="menu-link">
-        <i class="fa-solid fa-chalkboard-user fa-lg"></i>
-        <div data-i18n="Analytics">Kelas</div>
-      </a>
-    </li>
-
-    <!-- Soal -->
-    <li class="menu-item">
-      <a href="#" class="menu-link">
-        <i class="fa-solid fa-newspaper fa-lg"></i>
-        <div data-i18n="Analytics">Soal</div>
-      </a>
-    </li>
-
-    <!-- Nilai -->
-    <li class="menu-item">
-      <a href="#" class="menu-link">
-        <i class="fa-solid fa-chart-line fa-lg"></i>
-        <div data-i18n="Analytics">Nilai</div>
-      </a>
-    </li>
-
-    <!-- Bantuan -->
-    <li class="menu-item">
-      <a href="#" class="menu-link">
-        <i class="fa-regular fa-circle-question fa-lg"></i>
-        <div data-i18n="Analytics">Bantuan</div>
+        {{-- <i class="fa-solid fa-chalkboard-user fa-lg"></i> --}}
+        <div data-i18n="Analytics">Pasien</div>
       </a>
     </li>
 
